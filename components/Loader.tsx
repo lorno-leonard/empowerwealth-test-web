cbbef2c588af4af9043861262279b459d5c64cfb
Leonard Laput <lorno.leonard@gmail.com>
2021-11-09 23:35:27 +0800

    feat: update loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,8 +1,23 @@
 import React, { FC } from 'react'
 import { Spin } from 'antd'
+import styled from '@emotion/styled'
+
+import Container from '@components/Container'
+
+const LoaderContainer = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const Loader: FC = () => {
-  return <Spin size="large" />
+  return (
+    <Container>
+      <LoaderContainer>
+        <Spin size="large" />
+      </LoaderContainer>
+    </Container>
+  )
 }
 
 export default Loader
